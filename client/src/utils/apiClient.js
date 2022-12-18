ee956932dc98808847cb5755700293784b7051d3
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2022-12-18 03:43:01 +0100

    fix: api client error when returning errors

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -1,6 +1,6 @@
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
-const client = async (endpoint, { data, headers: customHeaders, ...customConfig } = {}) => {
+const apiClient = async (endpoint, { data, headers: customHeaders, ...customConfig } = {}) => {
   const clientConfig = {
     method: data ? 'POST' : 'GET',
     body: data ? JSON.stringify(data) : undefined,
@@ -12,12 +12,13 @@ const client = async (endpoint, { data, headers: customHeaders, ...customConfig 
   };
 
   return window.fetch(`${apiUrl}/${endpoint}`, clientConfig).then(async (response) => {
+    const data = await response.json();
     if (response.ok) {
-      return await response.json();
+      return data;
     } else {
-      return Promise.reject(data);
+      return Promise.reject(data.errors);
     }
   });
 };
 
-export { client };
+export { apiClient };
